7d64a7391830e1d5b956f70ccdb4d9a0556414b6
Aura <103134547+Djsce-Aura@users.noreply.github.com>
2022-05-01 11:25:05 +0530

    Add files via upload

---
+++ precache-manifest.ade8d3731e428f7ce1a008adceb6ff95.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a00ed4c997211e7ffdd101fe7ee9aeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56fddf7a2887082cecee",
+    "url": "/static/css/2.d1af52dc.chunk.css"
+  },
+  {
+    "revision": "56fddf7a2887082cecee",
+    "url": "/static/js/2.5d55b767.chunk.js"
+  },
+  {
+    "revision": "b47b83f293ef0a67523532a1d2a28ee0",
+    "url": "/static/js/2.5d55b767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b841794226cfb432f815",
+    "url": "/static/js/main.dbec91e4.chunk.js"
+  },
+  {
+    "revision": "c681ed3c3adb3f28e05b",
+    "url": "/static/js/runtime-main.8fec0d1a.js"
+  },
+  {
+    "revision": "017157151462074f299aa739901f28c8",
+    "url": "/static/media/QR.01715715.png"
+  },
+  {
+    "revision": "a7c7545e48d86cc370e58e447ef61e2a",
+    "url": "/static/media/aura.a7c7545e.png"
+  },
+  {
+    "revision": "ed4fc388227d6e63c5d33144abb99783",
+    "url": "/static/media/coming.ed4fc388.png"
+  },
+  {
+    "revision": "861caa544f610c349395dad121463aa8",
+    "url": "/static/media/cotm.861caa54.jpg"
+  },
+  {
+    "revision": "379cabeeca1c4ffacb72191eb1d53b01",
+    "url": "/static/media/dp.379cabee.jpg"
+  },
+  {
+    "revision": "269807bc10854a3a0513cc9f5aa557be",
+    "url": "/static/media/favicon.269807bc.ico"
+  },
+  {
+    "revision": "c455de894073ca03ba79a178d84da250",
+    "url": "/static/media/from-indu.c455de89.jpg"
+  },
+  {
+    "revision": "cf52eac1f888033da6124f36f8b2d373",
+    "url": "/static/media/streetplay.cf52eac1.jpg"
+  },
+  {
+    "revision": "69938ca50ca21178c943ed1a8d14f9ec",
+    "url": "/static/media/team1.69938ca5.jpg"
+  },
+  {
+    "revision": "1a507aef936054d71abd791b77e6d038",
+    "url": "/static/media/team2.1a507aef.jpg"
+  },
+  {
+    "revision": "7fe4d0e017883d76db14604fae373733",
+    "url": "/static/media/team3.7fe4d0e0.jpg"
+  },
+  {
+    "revision": "d3ebb8f15852ca5457ee740ec8673b3e",
+    "url": "/static/media/team4.d3ebb8f1.jpg"
+  },
+  {
+    "revision": "c8e3f820c577edf4539e6013ea4a5df6",
+    "url": "/static/media/team5.c8e3f820.jpg"
+  }
+]);
